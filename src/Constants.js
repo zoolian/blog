70cdf6aac2971aa5b0448f385843f16b03357bb2
zoolian <zoolian.creative@gmail.com>
2021-01-13 10:13:09 -0600

    typos

--- src/Constants.js
+++ src/Constants.js
@@ -1,8 +1,8 @@
-export const BLOG_API_URL = "https://api1.jmscottnovels.com:8080/blog"
-export const SECURITY_API_URL = "https://api1.jmscottnovels.com:8080/security"
-export const API_URL = "https://api1.jmscottnovels.com:8080"
+export const BLOG_API_URL = "https://api1.jmscottnovels.com/blog"
+export const SECURITY_API_URL = "https://api1.jmscottnovels.com/security"
+export const API_URL = "https://api1.jmscottnovels.com"
 export const USERNAME_ATTRIBUTE_NAME = 'authenticatedUser'
 export const DATE_ATTRIBUTE_NAME = 'date'
 export const BLOG_MANAGER_ID = '5fc54896e862b52cc709dbe8'
 export const POST_FORM_ID = '5fc66385e862b52cc709dbf0'
-export const RESET_PASSWORD_URL = 'https://security.jmscottnovels.com:3000/reset-request'
+export const RESET_PASSWORD_URL = 'https://security.jmscottnovels.com/reset-request'
